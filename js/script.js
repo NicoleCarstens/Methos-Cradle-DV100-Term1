d241a9d09e869640bfdfd34d66af90dec0c4153b
NicoleCarstens <125471810+NicoleCarstens@users.noreply.github.com>
2026-05-13 13:38:40 +0200

    Fixing contact form
    couldnt get the name to display

--- js/script.js
+++ js/script.js
@@ -18,5 +18,28 @@ function incCount(){
 
 }
 
+let form = document.forms['contactForm'];
+form.addEventListener("submit", getValues);
 
+function getValues(event){
+event.preventDefault();
+
+
+    let contactData = {
+        "name": document.getElementById('name').value,
+        "email": document.getElementById('email').value,
+        "subject": document.getElementById('subject').value,
+        "message": document.getElementById('message').value
+    };
+
+     console.log(contactData);
+
+     let output = `<p>Thank you <strong>${contactData.name}</strong>, we have received your message and will get back to you shortly.</p>`
+
+     document.querySelector(".code").innerHTML = output;
+     
+        
+form.reset();
+
+}
 
